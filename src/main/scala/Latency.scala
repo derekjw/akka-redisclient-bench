70967074df924478916bf8bd9b53f9bff901bc75
Derek Williams <derek@nebvin.ca>
2010-10-18 21:40:32 -0600

    Added (currently poorly named) latency bench

---
+++ src/main/scala/Latency.scala
@@ -0,0 +1,39 @@
+package net.fyrie.redis
+package bench
+
+import Commands._
+import serialization.Parse.Implicits._
+import se.scalablesolutions.akka.actor.Actor._
+import se.scalablesolutions.akka.dispatch._
+
+class AkkaLatencyBench(iterations: Int, connections: Int)(implicit conn: RedisClient) extends BenchIterations(iterations) {
+  val key = "akkalatencybench"
+
+  override def before {
+    conn send flushdb
+    conn send set(key, 0)
+    conn.resetStats
+  }
+  override def after {
+    conn.printStats
+    conn send flushdb
+  }
+
+  def run = {
+    val msg = incr(key)
+    val connIterations = iterations / connections
+    val totalIterations = connIterations * connections
+    val futures = (1 to connections).map(i => new DefaultCompletableFuture[Boolean](5000)).toList
+    val threads = futures map { f =>
+      new Thread {
+        override def run() {
+          (1 to connIterations) foreach {i => (conn send msg)}
+          f.completeWithResult(true)
+        }
+      }
+    }
+    threads.foreach(_.start())
+    futures.foreach(f => f.awaitBlocking)
+    assert ((conn send get[Int](key)).get == totalIterations)
+  }
+}
